e3fe63b43e83fba356c86e56d1d166348bf3b377
Nathan Fritz <fritzy@netflint.net>
2010-01-30 02:19:26 -0800

    fixed item table schema

--- sqliteschema.sql
+++ sqliteschema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE affiliation(id INTEGER PRIMARY KEY, node_id INTEGER, jid TEXT, type VARCHAR(10));
-CREATE TABLE item(id INTEGER PRIMARY KEY, node_id INTEGER, payload BLOB, time DATETIME, who TEXT);
+CREATE TABLE item(id INTEGER PRIMARY KEY, node_id INTEGER, name VARCHAR(255), payload BLOB, time DATETIME, who TEXT);
 CREATE TABLE node (id INTEGER PRIMARY KEY, name VARCHAR(255), type VARCHAR(100), config BLOB);
 CREATE TABLE roster(id INTEGER PRIMARY KEY, jid TEXT UNIQUE, subto INTEGER, subfrom INTEGER, jidto TEXT);
 CREATE TABLE subscription(id INTEGER PRIMARY KEY, node_id INTEGER, jid TEXT, type VARCHAR(10), config BLOB, subid VARCHAR(255), jidto TEXT);
