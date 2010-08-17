8e1237f5d52beb6e89038e4014a4d3deace6ee7d
fritzy <fritzy@ip-10-251-242-239.ec2.internal>
2010-08-17 04:38:32 +0000

    return from publish before notifying

--- sleekpubsub/node.py
+++ sleekpubsub/node.py
@@ -420,6 +420,10 @@ class BaseNode(object):
 	def publish(self, item, item_id=None, options=None, who=None):
 		if not item_id:
 			item_id = uuid.uuid4().hex
+		self.xmpp.schedule("%s::%s::publish" % (self.name, item_id), 0, self._publish, (item,), kwargs={'item_id': item_id, 'options': options, 'who': who})
+		return item_id
+
+	def _publish(self, item, item_id=None, options=None, who=None):
 		if item.tag == '{http://jabber.org/protocol/pubsub}item':
 			payload = item.getchildren()[0]
 		else:
@@ -438,7 +442,6 @@ class BaseNode(object):
 		max_items = int(self.config.get('pubsub#max_items', 0))
 		if max_items != 0 and len(self.itemorder) > max_items:
 			self.deleteItem(self.itemorder[0])
-		return item_id # item id
 
 	def deleteItem(self, id):
 		if id in self.items:
