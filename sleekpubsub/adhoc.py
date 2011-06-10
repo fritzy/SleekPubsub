09811d974c679a1b41fce01263c1339bbd6649eb
Nathan Fritz <nathan@andyet.net>
2011-06-10 04:14:34 +0000

    now works with latest SleekPubsub develop

--- sleekpubsub/adhoc.py
+++ sleekpubsub/adhoc.py
@@ -8,40 +8,40 @@ class PubsubAdhoc(object):
 		self.about = "Create and configure nodes, leafs, and add items."
 		self.pubsub = self.xmpp.plugin['xep_0060']
 		self.xform = self.xmpp.plugin['xep_0004']
-		self.adhoc = self.xmpp.plugin['xep_0050']
+		self.adhoc = self.xmpp.plugin['old_0050']
 		
 		createleaf = self.xmpp.plugin['xep_0004'].makeForm('form', "Create Leaf")
 		createleaf.addField('node', 'text-single')
-		self.xmpp.plugin['xep_0050'].addCommand('newleaf', 'Create Leaf', createleaf, self.createLeafHandler, True)
+		self.xmpp.plugin['old_0050'].addCommand('newleaf', 'Create Leaf', createleaf, self.createLeafHandler, True)
 
 		#createcollect = self.xmpp.plugin['xep_0004'].makeForm('form', "Create Collection")
 		#createcollect.addField('node', 'text-single', 'Node name')
-		#self.xmpp.plugin['xep_0050'].addCommand('newcollection', 'Create Collection', createcollect, self.createCollectionHandler, True)
+		#self.xmpp.plugin['old_0050'].addCommand('newcollection', 'Create Collection', createcollect, self.createCollectionHandler, True)
 
 		setitem = self.xmpp.plugin['xep_0004'].makeForm('form', "Set Item")
 		setitem.addField('node', 'text-single')
 		setitem.addField('id', 'text-single')
 		setitem.addField('xml', 'text-multi')
-		self.xmpp.plugin['xep_0050'].addCommand('setitem', 'Set Item', setitem, self.setItemHandler, True)
+		self.xmpp.plugin['old_0050'].addCommand('setitem', 'Set Item', setitem, self.setItemHandler, True)
 
 		remitem = self.xmpp.plugin['xep_0004'].makeForm('form', "Retract Item")
 		remitem.addField('node', 'text-single', 'Node name')
 		remitem.addField('id', 'text-single')
-		self.xmpp.plugin['xep_0050'].addCommand('remitem', 'Retract Item', remitem, self.retractItemHandler, True)
+		self.xmpp.plugin['old_0050'].addCommand('remitem', 'Retract Item', remitem, self.retractItemHandler, True)
 
 		
 		confnode = self.xmpp.plugin['xep_0004'].makeForm('form', "Configure Node")
 		confnode.addField('node', 'text-single')
-		self.xmpp.plugin['xep_0050'].addCommand('confnode', 'Configure Node', confnode, self.updateConfigHandler, True)
+		self.xmpp.plugin['old_0050'].addCommand('confnode', 'Configure Node', confnode, self.updateConfigHandler, True)
 		
 		subnode = self.xmpp.plugin['xep_0004'].makeForm('form', "Subscribe Node")
 		subnode.addField('node', 'text-single')
 		subnode.addField('jid', 'text-single')
-		self.xmpp.plugin['xep_0050'].addCommand('subnode', 'Subscribe Node', subnode, self.subscribeNodeHandler, True)
+		self.xmpp.plugin['old_0050'].addCommand('subnode', 'Subscribe Node', subnode, self.subscribeNodeHandler, True)
 	
 		delnode = self.xmpp.plugin['xep_0004'].makeForm('form', "Delete Node")
 		delnode.addField('node', 'text-single')
-		self.xmpp.plugin['xep_0050'].addCommand('delnode', 'Delete Node', delnode, self.deleteNodeHandler, True)
+		self.xmpp.plugin['old_0050'].addCommand('delnode', 'Delete Node', delnode, self.deleteNodeHandler, True)
 
 		affiliation = self.xmpp.plugin['xep_0004'].makeForm('form', "Change Affiliation")
 		affiliation.addField('node', 'text-single', 'Node name')
@@ -50,7 +50,7 @@ class PubsubAdhoc(object):
 		affs_list = ('owner', 'publisher', 'member', 'none', 'outcast')
 		for aff in affs_list:
 			affs.addOption(aff, aff.title())
-		self.xmpp.plugin['xep_0050'].addCommand('affiliation', 'Change Affiliation', affiliation, self.setAffiliation, True)
+		self.xmpp.plugin['old_0050'].addCommand('affiliation', 'Change Affiliation', affiliation, self.setAffiliation, True)
 		
 	def getStatusForm(self, title, msg):
 		status = self.xform.makeForm('form', title)
